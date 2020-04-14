422b11dbbe292d190603bfab3c74f2e500b4b1a9
James Cohen <jamesjcohen@gmail.com>
2020-04-14 13:52:22 +0100

    Added more stuff

--- lines_status_taskbar/Api.fs
+++ lines_status_taskbar/Api.fs
@@ -1,78 +1,53 @@
 ﻿module Api
 
 open System
-open FSharp.Control.Tasks
 open FSharp.Data
-
-//def station_arrivals(times):
-//    arrivals_string = ', '.join(map(str, times))
-//    if len(times) > 0:
-//        return arrivals_string
-//    else:
-//        return 'No trains'
-
-//def fastest_journey_time(from_post_code, to_post_code):
-//    req = requests.get( 'https://api.tfl.gov.uk/journey/journeyresults/' + from_post_code + '/to/' + to_post_code).json()
-//    journeys = req['journeys']
-//    times = sorted(list(map(lambda x: x['duration'],  journeys)))
-//    if len(times) > 0:
-//        return str(times[0])
-//    else:
-//        return str(-1)
-//        # todo: what to do about this case
-
-//let retrieveLineStatuses () =
-//    try
-//        let details =
-//            Document.Load("http://cloud.tfl.gov.uk/TrackerNet/LineStatus").LineStatuses
-//            |> Array.map (fun lineStatus -> (lineStatus.Line.Name, lineStatus.Status.Description))
-
-//        let (namePadding, descriptionPadding) =
-//            let names, descriptions = details |> Array.unzip
-//            let max (arr:string []) = arr |> Array.maxBy (fun x -> x.Length)
-//            (max names).Length, (max descriptions).Length
-
-//        let applyPadding ((str1:String),(str2:String)) =
-//            (str1.PadRight(namePadding, '.'),
-//             str2.PadRight(descriptionPadding, '.'))
-    
-
-//while true do
-//    let status = () |> retrieveLineStatuses
-    
-//    printToScreen 10 0 status
-//    |> ignore
-//    |> millisecondsUntilNextMinute
-//    |> Thread.Sleep
-
 
 type LineStatus = XmlProvider<"LineStatus.xml">
 type ArrivalTimes = JsonProvider<"arrivals.json">
-
-//def arrival_times(from_stn, to_stn):
-//    from_arrivals = requests.get( 'https://api.tfl.gov.uk/StopPoint/' + from_stn + '/arrivals').json()
-//    filtered_arrivals = list(filter(lambda x: x['destinationNaptanId'] == to_stn, from_arrivals))
-//    prettified_strings = list(map(lambda x: prettify_datetime(x['expectedArrival']),  filtered_arrivals))
-//    return list(filter(lambda x: x != "", prettified_strings))
+type Journeys = JsonProvider<"journey.json">
 
 let millisecondsUntilNextMinute () =
     ((60 - System.DateTime.Now.Second) * 1000) + System.DateTime.Now.Millisecond
 
-let liverpool_street_code = "910GLIVST"
-let liverpool_street_name = "Liverpool street"
-
-let forest_gate_code = "910GFRSTGT"
-let forest_gate_name = "Forest gate"
- 
-let gospel_oak_code = "910GGOSPLOK"
-let gospel_oak_name = "Gospel oak"
- 
-let wanstead_park_code = "910GWNSTDPK"
-let wanstead_park_name = "Wantead park"
+type Stations = 
+    | Liverpool_street
+    | Forest_gate
+    | Wantead_park
+    | Gospel_Oak
+    with
+    member this.ToStationCode =
+        match this with
+        | Liverpool_street -> "910GLIVST"
+        | Forest_gate      -> "910GFRSTGT"
+        | Wantead_park     -> "910GGOSPLOK"
+        | Gospel_Oak       -> "910GWNSTDPK"
+    override this.ToString() =
+        match this with
+        | Liverpool_street -> "Liverpool street"
+        | Forest_gate      -> "Forest gate"
+        | Wantead_park     -> "Gospel oak"
+        | Gospel_Oak       -> "Wanstead park"
+        
 
 let getData fromStation toStation =
     ArrivalTimes.Load(sprintf "https://api.tfl.gov.uk/StopPoint/%s/arrivals" fromStation)
-    |> Array.filter (fun x -> x.DestinationNaptanId = toStation)
-    |> Array.map (fun x ->
-        let f = DateTime.Now - x.ExpectedArrival.LocalDateTime
-        f.TotalMinutes |> int )
+    |> Array.choose (fun x ->
+        if x.DestinationNaptanId = toStation
+        then 
+            let f = DateTime.Now - x.ExpectedArrival.LocalDateTime
+            f.TotalMinutes |> int |> Some
+        else None)
+    |> Array.sort
+
+let getFastestJourneyTime fromPostCode toPostCode = async {
+        let! journeys = Journeys.AsyncLoad(sprintf "https://api.tfl.gov.uk/journey/journeyresults/%s/to/%s" fromPostCode toPostCode)
+
+        let fastest = 
+            journeys.Journeys
+            |> Array.map (fun x -> x.Duration)
+            |> Array.sort
+            |> Array.tryHead
+
+        return fastest
+    }
--- lines_status_taskbar/Program.fs
+++ lines_status_taskbar/Program.fs
@@ -5,14 +5,20 @@ open System.Windows
 open System.Drawing
 open System
 open System.Windows.Input
+open System.Windows.Threading
 
 type MainWindowViewModel(w,h) =
+    inherit ViewModule.ViewModelBase()
+
     let mutable width = w
     let mutable height = h
+    let mutable journeyTime = 0
     member __.Width with get() = width
     member __.Width with set (s:int) = ()
     member __.Height with get() = height
     member __.Height with set (s:int) = ()
+    member __.PrintJourneyTime = sprintf "%i mins" journeyTime
+    member __.JourneyTime with set s = journeyTime <- s
 
 let menuItem index text eventHandler = 
     let m = new System.Windows.Forms.MenuItem(Index = index, Text = text)
@@ -34,7 +40,22 @@ let main(_) =
     let window = Application.LoadComponent(new Uri("Status.xaml", UriKind.Relative)) :?> Window
     let width = 300
     let height = 600
-    window.DataContext <- (new MainWindowViewModel(width, height)) :> obj
+
+    let mainWindowViewModel = new MainWindowViewModel(width, height)
+    window.DataContext <- mainWindowViewModel
+
+    let update () = async{
+       let! fastest = Api.getFastestJourneyTime "SW1A1AA" "SW1A0AA"
+       System.Diagnostics.Debug.WriteLine(sprintf "%A" fastest)
+       match fastest with Some s -> mainWindowViewModel.JourneyTime <- s | None -> ()
+       window.DataContext <- mainWindowViewModel :> obj
+    }
+
+    let dispatcherTimer = new DispatcherTimer()
+    dispatcherTimer.Interval <- TimeSpan.FromSeconds(10.0)
+    dispatcherTimer.Tick.Add (fun _ -> update () |> Async.StartImmediate)
+    dispatcherTimer.Start()
+
 
     window.Left <- System.Windows.SystemParameters.WorkArea.Right - (width |> float)
     window.Top <- System.Windows.SystemParameters.WorkArea.Bottom - (height |> float)
@@ -54,6 +75,7 @@ let main(_) =
         if e.Button = MouseButtons.Left then
             if not window.IsVisible then
                 window.Visibility <- Visibility.Visible
+                window.Topmost <- true
             else
                 window.Visibility <- Visibility.Hidden
 
